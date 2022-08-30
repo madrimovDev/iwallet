a659e5f66e1385b5ad5a9a93f1d23b9835e76a22
madrimovDev <madrimov5014@gmail.com>
2022-08-30 14:45:25 +0500

    added chakra, redux

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,19 @@
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+
+import { theme } from '@configs'
+import store from '@store'
+
 const app = (
-  <>Hello World</>
+	<BrowserRouter>
+		<Provider store={store}>
+			<ChakraProvider theme={theme}>
+				Hello World
+				<ColorModeScript initialColorMode={theme.config.initialColorMode} />
+			</ChakraProvider>
+		</Provider>
+	</BrowserRouter>
 )
 
 export default app
--- vite.config.ts
+++ vite.config.ts
@@ -6,20 +6,20 @@ const pathResolver = (_path: string) => path.resolve(__dirname, `./src/${_path}`
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@app": pathResolver('app'), 
-      '@components': pathResolver('components'),
-      '@assets': pathResolver('assets'),
-      '@contexts': pathResolver('contexts'),
-      '@pages': pathResolver('pages'),
-      '@configs': pathResolver('configs'),
-      '@hooks': pathResolver('hooks'),
-      'store': pathResolver('store'),
-      '@utilities': pathResolver('utilities'),
-      '@layouts': pathResolver('layouts'),
-      '@data': pathResolver('data')
-    }
-  }
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@app': pathResolver('app'),
+			'@components': pathResolver('components'),
+			'@assets': pathResolver('assets'),
+			'@contexts': pathResolver('contexts'),
+			'@pages': pathResolver('pages'),
+			'@configs': pathResolver('configs'),
+			'@hooks': pathResolver('hooks'),
+			'@store': pathResolver('store'),
+			'@utilities': pathResolver('utilities'),
+			'@layouts': pathResolver('layouts'),
+			'@data': pathResolver('data')
+		}
+	}
 })
